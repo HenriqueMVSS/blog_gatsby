314baf94609b27429d8e662affacfb5a2c366618
Eduardo Velho <eduardo.velho@icloud.com>
2022-11-09 14:58:43 -0300

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,6 +5,7 @@ const config: GatsbyConfig = {
     title: `blog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
+  pathPrefix: "/blog_gatsby",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
