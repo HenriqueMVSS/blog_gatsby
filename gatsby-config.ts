f0cd91b998bb3393339f2d2c4183f94de2a9e99e
HenriqueMVSS <henrique_mateus1994@hotmail.com>
2022-11-15 02:22:41 -0300

    adding gatsby plugins

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,6 +12,30 @@ const config: GatsbyConfig = {
   graphqlTypegen: true,
   plugins: [
     "gatsby-plugin-styled-jsx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name:"data",
+        path:`${__dirname}/data`
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name:"posts",
+        path:`${__dirname}/posts`
+      }
+    },
+    {
+      resolve: "gatsby-transformer-json",
+      options: {
+        typeName:`Json`,
+      }
+    },
+    "gatsby-transformer-remark",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
   ],
 }
 
--- src/components/PostView.tsx
+++ src/components/PostView.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { graphql } from 'gatsby';
 
 export type PostView = {
     image:string;
